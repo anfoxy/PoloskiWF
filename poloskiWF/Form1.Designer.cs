75871b7784a2efe439e577e360175e4925c20376
Kazhaeva Anastasia <a.kazhaeva7@mail.ru>
2021-05-26 12:47:26 +0300

    first commit

--- poloskiWF/Form1.Designer.cs
+++ poloskiWF/Form1.Designer.cs
@@ -29,12 +29,69 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(50, 22);
+            this.toolStripButton1.Text = "start";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(0, 22);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
     }
 }
 
--- poloskiWF/Form1.cs
+++ poloskiWF/Form1.cs
@@ -12,9 +12,106 @@ namespace poloskiWF
 {
     public partial class Form1 : Form
     {
+       
+        List<strip> list = new List<strip>();
+        int n=0,r=0;
         public Form1()
         {
+            
             InitializeComponent();
+            MessageBox.Show("Правила игры: 1)когда на экране появится 10 полосок вы сможете их убирать. 2) чтобы убрать полоску кликните по ней левой кнопкой мыши. 3)вы победите когда не останется полосок.4)Вы проиграете если на экране окажется 35 полосок. ПРИЯТНОЙ ИГРЫ");
         }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+           
+        }
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((r == 1) &&(n==0))
+            {
+                timer1.Stop();
+                toolStripLabel1.Text = "0";
+                MessageBox.Show("ПОБЕДА ");
+
+            }
+            else {
+                toolStripLabel1.Text = "" + n;
+                n++;
+                //обработка сигналов таймера
+                list.Add(new strip());
+                Invalidate();
+                if (n > 35)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("ВЫ ПОГИБЛИ ");
+                }
+            }
+        }
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (r == 1)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].Click(e.X, e.Y))
+                    {
+                        bool over = false;
+                        for (int j = i + 1; j < list.Count; j++)
+                        {
+                            if (list[j].Over(list[i]))
+                            {
+                              
+                                over = true;
+                                break;
+                            }
+
+                        }
+                        if (over == false)
+                        {
+                            n--;
+                            list.RemoveAt(i);
+                            Invalidate();
+                            return;
+                        }
+                    }
+                }
+            }
+           
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (n > 10)
+            {
+                r = 1;
+                timer1.Interval = 700;
+            }
+            
+            Graphics g = this.CreateGraphics();
+           
+            for (int i = 0; i < list.Count; i++)
+            {
+                
+                list[i].Draw(g);
+            }
+            
+        }
+        
     }
 }
